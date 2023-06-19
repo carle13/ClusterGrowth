28c3a51c0d4a359fd105c8a7950af402755b3851
carle13 <carlerafael@outlook.com>
2023-06-19 17:41:48 +0200

    analysis smaller seeds and growth of 70 and 360 atoms seeds

--- plotGrowthSeeds.py
+++ plotGrowthSeeds.py
@@ -124,7 +124,7 @@ for d in directories:
         # plt.plot(t2['outputVoronoi/step900K'], nT['outputVoronoi/step900K'], label='900K')
         # plt.plot(t2s['outputVoronoi/step900Seeds/900K_1'], averageSeeds, label='Seeds 900')
         # plt.fill_between(t2s['outputVoronoi/step900Seeds/900K_1'], averageSeeds-deviationSeeds, averageSeeds+deviationSeeds, alpha=0.5, color='green')
-    plt.ylim([0, 550])
+    #plt.ylim([0, 550])
     plt.legend()
     plt.tight_layout()
     plt.savefig(d+'growthR1350K.png')
